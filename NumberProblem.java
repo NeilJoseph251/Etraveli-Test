ed231d2f225fd328905d73f64dd150fef1a5b9d0
NeilJoseph251 <101554675+NeilJoseph251@users.noreply.github.com>
2023-03-10 16:13:18 +0530

    Problem Solution

--- NumberProblem.java
+++ NumberProblem.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class NumberProblem 
@@ -7,16 +8,24 @@ public class NumberProblem
 		Scanner sc = new Scanner(System.in);
 		int[] a  = new int[3];
 		System.out.println("Enter 3 numbers: ");
-		for(int i=0;i<n;i++)
+		for(int i=0;i<3;i++)
 		{
 			a[i] = sc.nextInt();
 		}
 		System.out.println("Enter number N: ");
 		int n = sc.nextInt();
+		int count=0;
 		for(int i=0;i<3;i++)
 		{
-			
+			for(int j=i;j<3;j++)
+			{
+				if(a[i]+a[j]==n)
+					count++;
+			}
 		}
+		if(a[0]+a[1]+a[2]==n)
+			count++;
+		System.out.println("No of ways: "+count);
 		
 	}
 
